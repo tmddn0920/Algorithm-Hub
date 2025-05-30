c67ad1bdfeeec9a39e5e01fea6df8bbf1a8f2fbd
Seung._.rain <likepiano920@naver.com>
2025-05-30 16:10:38 +0900

    Add Code

---
+++ Bronze/Q_2754.py
@@ -0,0 +1,28 @@
+num = input()
+
+if num == 'A+':
+    print(4.3)
+elif num == 'A0':
+    print(4.0)
+elif num == 'A-':
+    print(3.7)
+elif num == 'B+':
+    print(3.3)
+elif num == 'B0':
+    print(3.0)
+elif num == 'B-':
+    print(2.7)
+elif num == 'C+':
+    print(2.3)
+elif num == 'C0':
+    print(2.0)
+elif num == 'C-':
+    print(1.7)
+elif num == 'D+':
+    print(1.3)
+elif num == 'D0':
+    print(1.0)
+elif num == 'D-':
+    print(0.7)
+elif num == 'F':
+    print(0.0)
