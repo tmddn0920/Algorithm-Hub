86b829b43c112cf6edf74fd602933db86f7e66a5
Seung._.rain <likepiano920@naver.com>
2025-08-19 18:43:19 +0900

    Add Codes

---
+++ Gold/Q_17387.py
@@ -0,0 +1,34 @@
+x1, y1, x2, y2 = map(int, input().split())
+x3, y3, x4, y4 = map(int, input().split())
+
+def CCW(x1, y1, x2, y2, x3, y3):
+    result = (x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3)
+    if result > 0:
+        return 1
+    elif result < 0:
+        return -1
+    return 0
+
+def isSame(x1, y1, x2, y2, x3, y3, x4, y4):
+    if min(x1, x2) <= max(x3, x4) and min(x3, x4) <= max(x1, x2) \
+        and min(y1, y2) <= max(y3, y4) and min(y3, y4) <= max(y1, y2):
+            return True
+    return False
+
+def isCross(x1, y1, x2, y2, x3, y3, x4, y4):
+    abc = CCW(x1, y1, x2, y2, x3, y3)
+    abd = CCW(x1, y1, x2, y2, x4, y4)
+    cda = CCW(x3, y3, x4, y4, x1, y1)
+    cdb = CCW(x3, y3, x4, y4, x2, y2)
+    if abc * abd == 0 and cda * cdb == 0:
+        return isSame(x1, y1, x2, y2, x3, y3, x4, y4)
+    elif abc * abd <= 0 and cda * cdb <= 0:
+        return True
+    return False
+
+result = isCross(x1, y1, x2, y2, x3, y3, x4, y4)
+
+if result:
+    print(1)
+else:
+    print(0)
